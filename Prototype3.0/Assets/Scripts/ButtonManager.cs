cb9f92aa28199c6a21db11dd20f02ad713688fda
Malte Hølledig <malte@holledig.dk>
2016-11-26 03:05:22 +0100

    Made MainMenu and LevelSelection. Just need to put in levels and maybe make background larger

---
+++ Prototype3.0/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void LoadScene(string sceneName) {
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void ExitApplication() {
+		Application.Quit();
+	}
+
+}
