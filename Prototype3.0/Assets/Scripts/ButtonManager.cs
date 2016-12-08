3782ea493a8b73ea6f589051bbdd66c654cc10ab
Hubchris <Gwanplants@Christophers-iMac.local>
2016-12-08 15:44:00 +0100

    changes to all scenes, setup level selection, tutotial integrated

--- Prototype3.0/Assets/Scripts/ButtonManager.cs
+++ Prototype3.0/Assets/Scripts/ButtonManager.cs
@@ -5,12 +5,38 @@ using System.Collections;
 
 public class ButtonManager : MonoBehaviour {
 
-	public void LoadScene(string sceneName) {
+	//For loading
+	public Slider loadingbar;
+	public GameObject loadingImage;
+	private AsyncOperation async;
+
+	public void LoadScene(string sceneName) { //Loads scene with a loadscreen
+		//SceneManager.LoadScene(sceneName);
+
+		GetLevel(sceneName);
+	}
+	public void LoadSceneNoScreen(string sceneName){ //Loads scene without a loadscreen
 		SceneManager.LoadScene(sceneName);
 	}
 
 	public void ExitApplication() {
 		Application.Quit();
+	}
+		
+	public void GetLevel(string level)
+	{
+		loadingImage.SetActive(true);
+		StartCoroutine(LoadLevelWithBar(level));
+	}
+
+	IEnumerator LoadLevelWithBar (string level)
+	{
+		async = Application.LoadLevelAsync(level);
+		while (!async.isDone)
+		{
+			loadingbar.value = async.progress;
+			yield return null;
+		}
 	}
 
 }
---
+++ Prototype3.0/Assets/Tutorial.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+	public Image uiImage;
+	public bool tutorialEnabled;
+
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+		if (tutorialEnabled)
+		{
+			uiImage.gameObject.SetActive(true);
+			if (Input.GetKeyDown(KeyCode.Return))
+				{
+					tutorialEnabled = false;
+					uiImage.gameObject.SetActive(false);
+					gameObject.SetActive(false);
+				}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			tutorialEnabled = true;
+		}
+	}
+}
