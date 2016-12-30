a998ee8b794ee99c76e79530b7846239e34387fd
Hubchris <Gwanplants@Christophers-iMac.local>
2016-12-30 15:30:07 +0100

    trying out new level design stuff in LevelDesignTest scene, added Secret area reveal script

---
+++ Prototype3.0/Assets/SecretArea.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretArea : MonoBehaviour {
+
+	public SpriteRenderer spriteRend;
+	private Color color;
+	private bool revealing;
+
+	void Start () 
+	{
+		spriteRend = GetComponentInChildren<SpriteRenderer>();
+		color = spriteRend.material.color;
+	}
+	
+
+	void Update () 
+	{
+		if (revealing)
+		{
+			Reveal();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player")
+		{
+			revealing = true;
+		}
+	}
+
+	void Reveal()
+	{
+		Debug.Log("Start fading");
+		color.a -= 0.05f;
+		spriteRend.material.color = color;
+	}
+}
