910a4cfa6c6cd36e01b20948500f39efadb4173d
Hubchris <Gwanplants@Christophers-iMac.local>
2017-01-12 18:51:08 +0100

    created the Conveyor belt - it works for player and objects with rigidbody

--- Prototype3.0/Assets/Scripts/ConveyorBelt.cs
+++ Prototype3.0/Assets/Scripts/ConveyorBelt.cs
@@ -12,37 +12,93 @@ public class ConveyorBelt : MonoBehaviour {
 //	private Vector2 moveRight = new Vector2(1,0);
 
 	public List<Transform> movedObjects = new List<Transform>();
-	public bool moving;
+	public float playerSpeed;
+	public float objectSpeed;
+	public bool right, left;
+
+	private bool moving, movingPlayer;
+	public Vector3 direction;
+	private Bounds bounds;
+	void Start()
+	{
+		bounds = GetComponent<BoxCollider2D>().bounds;
+		bounds.Expand(new Vector3(20f,0,0));
+	}
+
 	void FixedUpdate()
 	{
 
 		if (moving)
 		{
-			for (int i = 0; i < movedObjects.Count; i++)
+			if (right)
 			{
-				movedObjects[i].position = Vector3.Lerp(movedObjects[i].position, Vector3.right, 0.05f*Time.deltaTime);
+				direction = new Vector3(bounds.max.x, bounds.max.y, 0f);
+				MoveObjects(direction);
+			}
+			if(left)
+			{
+				direction = new Vector3(bounds.min.x, bounds.max.y, 0f);
+				MoveObjects(direction);
 			}
 		}
+		
 //		Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 //		rigidbody.position -= moveRight * speed * Time.deltaTime;
 //		rigidbody.MovePosition (rigidbody.position + moveRight * speed * Time.deltaTime);
 //		
 //		distanceTravelled += speed * Time.deltaTime;
 	}
+
+	void Update()
+	{
+		if (movingPlayer)
+		{
+			MovePlayer();
+		}
+	}
+
+	/// Collision Enter
 	void OnCollisionEnter2D(Collision2D other)
 	{
-		if (other.gameObject.name == "Player" || other.gameObject.tag == "DynamicParticle")
-		{
-			movedObjects.Add(other.transform);
-			moving = true;
-		}
+		movedObjects.Add(other.transform);
+		moving = true;
 	}
+
+	/// Collision Exit
 	void OnCollisionExit2D(Collision2D other)
 	{
-		if (other.gameObject.name == "Player" || other.gameObject.tag == "DynamicParticle")
+		movedObjects.Remove(other.transform);
+	}
+
+	/// Trigger Enter
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player")
 		{
-			movedObjects.Remove(other.transform);
+			movingPlayer = true;
 		}
 	}
-	
+
+	/// Trigger Exit
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player")
+		{
+			movingPlayer = false;
+		}
+	}
+
+	void MovePlayer()
+	{
+		Player.instance.gameObject.transform.position += transform.right * playerSpeed * Time.deltaTime;
+		Debug.Log("MovingPlyer on ConveyorBelt");
+	}
+
+	void MoveObjects(Vector3 direction)
+	{
+		for (int i = 0; i < movedObjects.Count; i++)
+		{
+			movedObjects[i].position = Vector3.Lerp(movedObjects[i].position, direction, objectSpeed * Time.deltaTime);
+		}
+	}
 }
