743d8f60c33e7eb7d310c1bb6eba26f02bac9579
Malte Hølledig <malteholledig@Maltes-MacBook-Pro.local>
2017-02-04 02:08:43 +0100

    Made tile art

---
+++ Prototype3.0/Assets/Scripts/Background.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	SpriteRenderer[] middleLayer;
+
+	// Use this for initialization
+	void Start () {
+		middleLayer = this.transform.GetComponentInChildren<SpriteRenderer> ();
+		for (int i = 0; middleLayer.Length < i; i++) {
+			middleLayer
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
