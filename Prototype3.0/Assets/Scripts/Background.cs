498394931ff8d808ccdae95c01e8db41534d4682
Malte Hølledig <malteholledig@Maltes-MacBook-Pro.local>
2017-02-05 19:06:17 +0100

    Made parallax background

--- Prototype3.0/Assets/Scripts/Background.cs
+++ Prototype3.0/Assets/Scripts/Background.cs
@@ -4,18 +4,58 @@ using UnityEngine;
 
 public class Background : MonoBehaviour {
 
-	SpriteRenderer middleLayer;
+	public float backgroundSize;
+	public float parallaxSpeed;
 
-	// Use this for initialization
-	void Start () {
-		middleLayer = this.transform.GetComponentInChildren<SpriteRenderer> ();
-		/*for (int i = 0; middleLayer.Length < i; i++) {
-			middleLayer
-		}*/
+	private Transform cameraTransform;
+	private Transform[] layers;
+	private float viewZone = 10;
+	private int leftIndex;
+	private int rightIndex;
+	private float lastCameraX;
+
+	private void Start() {
+		cameraTransform = GameObject.Find("PlayerCamera").transform;
+		lastCameraX = cameraTransform.position.x;
+		layers = new Transform[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++) {
+			layers [i] = transform.GetChild (i);
+		}
+		leftIndex = 0;
+		rightIndex = layers.Length - 1;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Update() {
+		float deltaX = cameraTransform.position.x - lastCameraX;
+		transform.position += Vector3.right * (deltaX * parallaxSpeed);
+		lastCameraX = cameraTransform.position.x;
+
+		if (cameraTransform.position.x < (layers [leftIndex].transform.position.x + viewZone)) {
+			ScrollLeft();
+		}
+		if (cameraTransform.position.x > (layers [rightIndex].transform.position.x - viewZone)) {
+			ScrollRight();
+		}	
 	}
+
+	private void ScrollLeft() {
+		int lastRight = rightIndex;
+		layers [rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+		leftIndex = rightIndex;
+		rightIndex--;
+		if (rightIndex < 0) {
+			rightIndex = layers.Length - 1;
+		}
+	}
+
+	private void ScrollRight() {
+		int lastLeft = leftIndex;
+		layers [leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+		rightIndex = leftIndex;
+		leftIndex++;
+		if (leftIndex == layers.Length) {
+			leftIndex = 0;
+		}
+	}
+
 }
