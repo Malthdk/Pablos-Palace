bfadfed696e2ba66fba6424f2c54b5db20669c9f
Hubchris <Gwanplants@Christophers-iMac.local>
2017-02-15 19:21:49 +0100

    New paint obsticles, new level changes to lvl 3, new particle drip

---
+++ Prototype3.0/Assets/BlackPaintAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackPaintAnimation : MonoBehaviour {
+
+	public float rotateRate = 1.0f;
+	public float invokeRate = 1.0f;
+
+	float xRot;
+	float yRot;
+	float zRot;
+	float wRot;
+
+	void Start () 
+	{
+		InvokeRepeating("newRotation", 0.0f, invokeRate);	
+	}
+	
+
+	void Update () 
+	{
+		Quaternion RandomQuat = new Quaternion(xRot, yRot, zRot, wRot);
+		transform.rotation = Quaternion.Slerp(transform.rotation, RandomQuat, rotateRate * Time.deltaTime);
+	}
+
+	void newRotation ()
+	{
+		xRot = Random.Range(-0, 0);
+		yRot = Random.Range(-0, 0);
+		zRot = Random.Range(-45, 45);
+		wRot = Random.Range(-45, 45);
+	}
+}
