ada36fc9ebf781d5dcd738a2ff02617fdd7890e6
Hubchris <Gwanplants@Christophers-iMac.local>
2017-05-03 16:32:42 +0200

    create the On/off paper mechanic and experimented with new ideas.

---
+++ Prototype3.0/Assets/Scripts/OnOffPaper.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffPaper : MonoBehaviour {
+
+	//publics
+	public float turnTime;
+	public float startDelay;
+	public OnAndOff onAndOff;
+
+	//privates
+	private MeshRenderer myRenderer;
+	private float tempTurnTime;
+
+	public enum OnAndOff
+	{
+		On,
+		Off
+	}
+
+	void Start () 
+	{
+		myRenderer = gameObject.GetComponent<MeshRenderer>();
+		tempTurnTime = turnTime;
+	}
+
+	void Update () 
+	{
+		switch (onAndOff)
+		{
+		case OnAndOff.On:
+
+			myRenderer.material.color = Color.white;
+			gameObject.layer = 14;
+
+			tempTurnTime -= Time.deltaTime;
+			if (tempTurnTime <= 0)
+			{
+				onAndOff = OnAndOff.Off;
+				tempTurnTime = turnTime;
+			}
+			break;
+
+		case OnAndOff.Off:
+
+			myRenderer.material.color = Color.grey;
+			gameObject.layer = 0;
+
+			tempTurnTime -= Time.deltaTime;
+			if (tempTurnTime <= 0)
+			{
+				onAndOff = OnAndOff.On;
+				tempTurnTime = turnTime;
+			}
+			break;
+		}
+	}
+
+	IEnumerator StartDelay()
+	{
+		yield return new WaitForSeconds(startDelay);
+
+		onAndOff = OnAndOff.On;
+	}
+		
+}
